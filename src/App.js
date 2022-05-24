0486c7ef1894bb0f199eca265f0698fe75256631
Jenny Chau <jnechau@gmail.com>
2022-05-24 18:57:28 -0400

    customized google font to Mulish, other nice option Nunito

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import NewIssue from './NewIssue'
 //graphics
 import logoE from './graphics/logoE.png'
 //css
+import { createTheme, ThemeProvider } from '@mui/material'
 import './estimator.css'
+
+const jenny = createTheme ( {
+  typography: {
+    fontFamily: "Mulish"
+  }
+} )
 
 const App = () => {
   return (
-    <div>
-
-      <img className="logo" src={logoE} alt="logo in green" />
-      <NewIssue />
-    </div>
+    <ThemeProvider theme={jenny}>
+      <div>
+        <img className="logo" src={logoE} alt="logo in green" />
+        <NewIssue />
+      </div>
+    </ThemeProvider>
   );
 }
 
