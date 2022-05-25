d13f356e6ccf25c9ee96aeeac852c10fa4d81542
Jenny Chau <jnechau@gmail.com>
2022-05-24 22:49:38 -0400

    switched radio status from item to issue

--- src/NewIssue.js
+++ src/NewIssue.js
@@ -1,10 +1,24 @@
+//libraries
+import { useState } from 'react';
+
 //components
 import NewItem from './NewItem';
 //css
 import TextField from '@mui/material/TextField';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
 
 
 const NewIssue = () => {
+
+    const [infoRadio, setInfoRadio] = useState("");
+    const [radioStatus, setRadioStatus] = useState("proactive");
+
+    const handleRadioChange = (e) => {
+        setRadioStatus(e.target.value);
+    };
+
+    console.log(radioStatus)
 
 
     return (
@@ -18,10 +32,40 @@ const NewIssue = () => {
                     variant="outlined"
                 />
 
+                <div className="item-status">
+
+                    <RadioGroup row name="row-radio-buttons-group" >
+
+                        <Radio
+                            color="success"
+                            onClick={() => setInfoRadio("Proactive")}
+                            onChange={handleRadioChange}
+                            value="proactive"
+                        />
+                        <Radio
+                            color="warning"
+                            onClick={() => setInfoRadio("Usable")}
+                            onChange={handleRadioChange}
+                            value="usable"
+                        />
+                        <Radio
+                            color="error"
+                            onClick={() => setInfoRadio("Unusable")}
+                            onChange={handleRadioChange}
+                            value="unusable"
+                        />
+
+
+                    </RadioGroup>
+
+                </div>
+
+                <p className="info-radio"> {infoRadio} </p>
+
                 <br />
 
-                    <NewItem/>
-                    <NewItem/>
+                <NewItem />
+                <NewItem />
 
 
             </form>
--- src/NewItem.js
+++ src/NewItem.js
@@ -1,21 +1,8 @@
-//libraries
-import { useState } from 'react';
-
 //css
 import TextField from '@mui/material/TextField';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
+
 
 const NewItem = () => {
-
-    const [infoRadio, setInfoRadio] = useState("");
-    const [radioStatus, setRadioStatus] = useState("proactive");
-
-    const handleChange = (e) => {
-        setRadioStatus(e.target.value);
-    };
-
-
 
     return (
         <>
@@ -28,39 +15,6 @@ const NewItem = () => {
                         variant="outlined"
                     />
                 </div>
-
-                <div className="item-status">
-
-                    <RadioGroup
-                        row
-                        name="row-radio-buttons-group"
-                    >
-
-                        <Radio 
-                        color="success" 
-                        onClick={() => setInfoRadio("Proactive")}
-                        onChange={handleChange}
-                        value="proactive"
-                        />
-                        <Radio 
-                        color="warning"
-                        onClick={() => setInfoRadio("Usable")}
-                        onChange={handleChange}
-                        value="usable"
-                        />
-                        <Radio
-                        color="error" 
-                        onClick={() => setInfoRadio("Unusable")}
-                        onChange={handleChange}
-                        value="unusable"
-                        />
-
-
-                    </RadioGroup>
-
-                </div>
-
-                <p className="info-radio"> {infoRadio} </p>
 
 
             </div>
